408873300877d56e77270b73bb3c0004d2828030
omodauda <omodauda.dl@gmail.com>
2023-03-09 15:46:01 +0100

    Delay splash screen on app ready

--- App.tsx
+++ App.tsx
@@ -1,7 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hideAsync()
+    }, 2000)
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
