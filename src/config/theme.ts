541e991f7b593df97436753a1a8295550c6ac2cd
omodauda <omodauda.dl@gmail.com>
2023-03-11 03:03:14 +0100

    theme with react-native-paper & react-navigation

---
+++ src/config/theme.ts
@@ -0,0 +1,41 @@
+import { adaptNavigationTheme, configureFonts, MD3DarkTheme, MD3LightTheme } from 'react-native-paper';
+import { lightColors, darkColors } from './colors';
+import fontConfig from './fonts';
+import { DarkTheme as NavigationDarkTheme, DefaultTheme as NavigationDefaultTheme } from '@react-navigation/native';
+
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+  reactNavigationLight: NavigationDefaultTheme,
+  reactNavigationDark: NavigationDarkTheme,
+});
+
+const CombinedDefaultTheme = {
+  ...MD3LightTheme,
+  ...LightTheme,
+  colors: {
+    ...MD3LightTheme.colors,
+    ...LightTheme.colors,
+    ...lightColors
+  }
+}
+
+const CombinedDarkTheme = {
+  ...MD3DarkTheme,
+  ...DarkTheme,
+  colors: {
+    ...MD3DarkTheme.colors,
+    ...DarkTheme.colors,
+    ...darkColors
+  }
+}
+
+const fonts = configureFonts({ config: fontConfig })
+
+export const AppDefaultTheme = {
+  ...CombinedDefaultTheme,
+  fonts
+}
+
+export const AppDarkTheme = {
+  ...CombinedDarkTheme,
+  fonts
+}
