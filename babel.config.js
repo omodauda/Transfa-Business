b857d1c024949ca7f99c36ad9a8131d3a2acf328
omodauda <omodauda.dl@gmail.com>
2023-03-11 03:02:03 +0100

    configure module resolver

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,18 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        require.resolve("babel-plugin-module-resolver"),
+        {
+          root: ["./"],
+          alias: {
+            "^~(.+)": "./src/\\1",
+            assets: "./assets",
+          },
+        },
+      ],
+    ],
     env: {
       production: {
         plugins: ["react-native-paper/babel"],
