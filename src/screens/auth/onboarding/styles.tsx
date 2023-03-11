cc6571c8ecb804eb032ab411965b80bc85278f34
omodauda <omodauda.dl@gmail.com>
2023-03-11 16:18:11 +0100

    onboarding screen styles

---
+++ src/screens/auth/onboarding/styles.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1
+  },
+  gradientView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 24,
+  },
+})
+
+export default styles
