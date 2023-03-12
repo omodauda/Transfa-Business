7bc49052e5ba54f1e3446125d151511be15f20c5
omodauda <omodauda.dl@gmail.com>
2023-03-12 03:16:49 +0100

    SafeArea Screen props type

---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export type JustifyContent =
+  'center' | 'flex-end' | 'flex-start' | 'space-around' | 'space-between' | 'space-evenly'
+
+export type AlignItems =
+  'baseline' | 'center' | 'flex-end' | 'flex-start' | 'stretch'
