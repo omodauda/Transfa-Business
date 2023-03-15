6ca5e9f61463523669f1fdabf6ed233cf76d0a78
omodauda <omodauda.dl@gmail.com>
2023-03-15 19:29:40 +0100

    add param to signup screen

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -2,7 +2,7 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 export type RootStackParamList = {
   Onboarding: undefined;
-  SignUp: undefined;
+  SignUp: { step: number };
   Login: undefined;
   ForgotPassword: undefined;
   Verification: undefined;
