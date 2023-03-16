957639a1fe098e2f3f42b5422b81639d82097332
omodauda <omodauda.dl@gmail.com>
2023-03-17 00:52:04 +0100

    add forgot password screen to navigator

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -6,6 +6,7 @@ import { RootStackParamList } from '~types/navigation';
 import Onboarding from './auth/onboarding';
 import Login from './auth/login';
 import Signup from './auth/signup';
+import ForgotPassword from './auth/forgotPassword';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -16,6 +17,7 @@ export default function Screens() {
         <Stack.Screen name='Onboarding' component={Onboarding} />
         <Stack.Screen name='Login' component={Login} />
         <Stack.Screen name='SignUp' component={Signup} />
+        <Stack.Screen name='ForgotPassword' component={ForgotPassword} />
       </Stack.Group>
     </Stack.Navigator>
   )
