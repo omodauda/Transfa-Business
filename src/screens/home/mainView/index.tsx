7d7e61ca605e1737c29d9396c0d60308321d3c3f
omodauda <omodauda.dl@gmail.com>
2023-03-23 17:58:43 +0100

    home screen active view

---
+++ src/screens/home/mainView/index.tsx
@@ -0,0 +1,64 @@
+/* eslint-disable global-require */
+import {View} from 'react-native'
+import React from 'react'
+import CustomHeader from '~components/CustomHeader'
+import SettingSvg from '~components/svg/setting'
+import { useTheme, Text } from 'react-native-paper'
+import {BIKES, ORDERS} from '~mocks'
+import Button from '~components/Button'
+import styles from './styles'
+import ActiveBikesList from './ActiveBikesList'
+import RecentFulfiledOrderList from './RecentFulfiledOrderList'
+
+export default function MainView() {
+  const { colors } = useTheme()
+  const enableViewAll = ORDERS.length > 3;
+
+  const recentOrders = ORDERS.slice(0, 3)
+  return (
+    <View style={styles.screen}>
+      <View style={styles.paddedView}>
+        <CustomHeader
+        title='Transfa'
+        icon={<SettingSvg />}
+        onIconPress={() => { }}
+        labelStyle={{color: colors.primary}}
+        />
+        <Text
+          variant='bodyMedium'
+          style={[styles.welcomeText, {color: colors.onBackground}]}
+        >
+          Welcome,
+          <Text
+            variant="labelMedium"
+            style={{color: colors.primary}}
+          > Admin</Text>
+        </Text>
+        <View style={[styles.bikeView]}>
+          <Text
+            variant="labelMedium"
+            style={{ color: colors.onBackground, marginBottom: 16 }}
+          >
+            ACTIVE BIKES
+          </Text>
+          <ActiveBikesList data={BIKES} />
+        </View>
+      </View>
+
+      <View style={[styles.paddedView, styles.recentOrders, {backgroundColor: colors.tertiary}]}>
+        <View style={styles.spacedRow}>
+          <Text variant="bodySmall" style={{ color: colors.onBackground, fontFamily: 'GeneralSans-Medium' }}>
+            RECENT ORDER FULFILMENTS
+          </Text>
+          {enableViewAll && (
+            <Text variant="titleSmall" style={{ color: colors.primary }}>
+              View All
+            </Text>)
+          }
+        </View>
+        <RecentFulfiledOrderList data={recentOrders} />
+        <Button label='Add A New Bike' />
+      </View>
+    </View>
+  )
+}
