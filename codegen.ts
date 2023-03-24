a73b955beee9b65567c684a09c7d515a4a0f1c57
omodauda <omodauda.dl@gmail.com>
2023-03-24 01:29:56 +0100

    update codegen documents extension

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   schema: 'https://transfa-staging.fly.dev/graphql',
-  documents: ['src/graphql/queries/api/*.tsx'],
+  documents: ['src/graphql/queries/api/*.ts'],
   generates: {
     'src/__generated__/': {
       preset: 'client',
