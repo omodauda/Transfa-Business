54ff7d25691c329e4e8aae0769a18f092226fe45
omodauda <omodauda.dl@gmail.com>
2023-03-24 01:33:58 +0100

    show auth stack group

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -3,12 +3,12 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootStackParamList } from '~types/navigation';
 
 // screens
-// import Onboarding from './auth/onboarding';
-// import Login from './auth/login';
-// import Signup from './auth/signup';
-// import ForgotPassword from './auth/forgotPassword';
-// import Verification from './auth/verification';
-// import ResetPassword from './auth/resetPassword';
+import Onboarding from './auth/onboarding';
+import Login from './auth/login';
+import Signup from './auth/signup';
+import ForgotPassword from './auth/forgotPassword';
+import Verification from './auth/verification';
+import ResetPassword from './auth/resetPassword';
 import Home from './home';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -16,14 +16,14 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export default function Screens() {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      {/* <Stack.Group>
+      <Stack.Group>
         <Stack.Screen name='Onboarding' component={Onboarding} />
         <Stack.Screen name='Login' component={Login} />
         <Stack.Screen name='SignUp' component={Signup} />
         <Stack.Screen name='ForgotPassword' component={ForgotPassword} />
         <Stack.Screen name='Verification' component={Verification} />
         <Stack.Screen name='ResetPassword' component={ResetPassword} />
-      </Stack.Group> */}
+      </Stack.Group>
       <Stack.Group>
         <Stack.Screen name="Home" component={Home} />
       </Stack.Group>
