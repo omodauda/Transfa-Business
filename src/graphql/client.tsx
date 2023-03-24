e7c66ba36bd7d2cd1160a925096f2a73d773af40
omodauda <omodauda.dl@gmail.com>
2023-03-24 01:34:14 +0100

    add error link to client

--- src/graphql/client.tsx
+++ src/graphql/client.tsx
@@ -6,6 +6,7 @@ import {
 } from '@apollo/client';
 import { CachePersistor, AsyncStorageWrapper } from 'apollo3-cache-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {onError} from '@apollo/client/link/error'
 
 
 export const cache = new InMemoryCache();
@@ -19,8 +20,23 @@ const httpLink = new HttpLink({
   uri: 'https://transfa-staging.fly.dev/graphql'
 })
 
+const errorLink = onError(({graphQLErrors}) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach?.(e => {
+      console.log(e.message)
+      // showMessage({
+      //   message: 'Error',
+      //   description: e.message,
+      //   type: 'default',
+      //   backgroundColor: Colors.purple,
+      //   color: Colors.white,
+      // });
+    });
+  }
+});
+
 const client = new ApolloClient({
-  link: from([httpLink]),
+  link: from([errorLink, httpLink]),
   cache
 })
 
