6775d3eb013ea521345ec5e97d922f651012dbcb
omodauda <omodauda.dl@gmail.com>
2023-03-27 15:42:17 +0100

    save token to client on successful register

--- src/hooks/api/useRegister.ts
+++ src/hooks/api/useRegister.ts
@@ -1,12 +1,25 @@
 import { useMutation } from '@apollo/client';
+import client from '~graphql/client';
 import register from '~graphql/queries/api/register';
+import AUTH_STATE from '~graphql/queries/app';
 import { RegisterBusinessInput } from '~__generated__/graphql';
 
 export default function useRegister() {
   const [mutate, { loading }] = useMutation(register, {
     onCompleted: result => {
-      console.log('register.result', result)
+      if (result.registerBusiness.token) {
+        const { token } = result.registerBusiness;
+        client.writeQuery({
+          query: AUTH_STATE,
+          data: {
+            auth: {
+              accessToken: token
+            }
+          }
+        })
+      }
     },
+    onError: () => { }
   });
 
   const signUp = (input: RegisterBusinessInput) =>
