789a93d72812b58aa6c50ada47ae2feb2db78df1
omodauda <omodauda.dl@gmail.com>
2023-03-28 16:03:55 +0100

    forgot password phone mutatation types

--- src/__generated__/gql.ts
+++ src/__generated__/gql.ts
@@ -13,6 +13,7 @@ import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
+    "\n  mutation ForgotPasswordPhone($input: PhoneInput!) {\n    forgotPasswordPhone(input: $input) {\n      message\n    }\n  }\n": types.ForgotPasswordPhoneDocument,
     "\n  mutation Login($input: LoginWithEmailInput!) {\n    loginBusiness(input: $input) {\n      token\n    }\n  }\n": types.LoginDocument,
     "\n  mutation Register($input: RegisterBusinessInput!) {\n    registerBusiness (input: $input) {\n      token\n    }\n  }\n": types.RegisterDocument,
 };
@@ -31,6 +32,10 @@ const documents = {
  */
 export function gql(source: string): unknown;
 
+/**
+ * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function gql(source: "\n  mutation ForgotPasswordPhone($input: PhoneInput!) {\n    forgotPasswordPhone(input: $input) {\n      message\n    }\n  }\n"): (typeof documents)["\n  mutation ForgotPasswordPhone($input: PhoneInput!) {\n    forgotPasswordPhone(input: $input) {\n      message\n    }\n  }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
