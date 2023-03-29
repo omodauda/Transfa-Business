23915878fa679b263710639ce92ade7b591b6ce3
omodauda <omodauda.dl@gmail.com>
2023-03-29 11:25:49 +0100

    reset password with phone

--- src/screens/auth/resetPassword/index.tsx
+++ src/screens/auth/resetPassword/index.tsx
@@ -10,48 +10,44 @@ import ResetPasswordForm from '~components/form/ResetPassword';
 import { ResetPasswordInput } from '~types';
 import CustomHeader from '~components/CustomHeader';
 import BackButtonSvg from '~components/svg/backbutton';
+import useResetPasswordPhone from '~hooks/api/useResetPasswordPhone';
+import { showMessage } from 'react-native-flash-message';
 
 export default function ResetPassword ({
   navigation,
+  route: {
+    params: {data: {type, value: {data, otp}}}
+  }
 }: RootStackScreenProps<'ResetPassword'>) {
   const { colors } = useTheme();
   
   const submitForm = ({ password }: ResetPasswordInput) => {
-     navigation.navigate('Login')
-    // console.log({email, password})
+    if (type === 'phone') {
+      resetPasswordPhone({phone: data, otp, newPassword: password});
+    } else if (type === 'email') {
+      // resetPasswordEmail({email: data, otp, newPassword});
+    }
   }
 
-  // const success = (msg: string) => {
-  //   showMessage({
-  //     message: 'New Password Created',
-  //     description: msg,
-  //     type: 'default',
-  //     backgroundColor: colors.primary,
-  //     color: Colors.white,
-  //   });
-  //   setTimeout(() => {
-  //     navigation.reset({
-  //       index: 0,
-  //       routes: [{name: 'Onboarding'}],
-  //     });
-  //   }, 3500);
-  // };
+  const success = (msg: string) => {
+    showMessage({
+      message: 'New Password Created',
+      description: msg,
+      type: 'default'
+    });
+    setTimeout(() => {
+      navigation.reset({
+        index: 0,
+        routes: [{name: 'Onboarding'}],
+      });
+    }, 3500);
+  };
 
-  // const {resetPassword: resetPasswordPhone, loading: withPhoneLoading} =
-  //   useResetPasswordPhone({success});
+  const {resetPassword: resetPasswordPhone, loading: withPhoneLoading} =
+    useResetPasswordPhone({success});
 
   // const {resetPassword: resetPasswordEmail, loading: withEmailLoading} =
   //   useResetPasswordEmail({success});
-
-  // const handleSubmit = (newPassword: string) => {
-  //   // if (type === 'phone') {
-  //   //   // const phone = IntlFormat(data);
-  //   //   // resetPasswordPhone({phone, otp, newPassword});
-  //   // } else if (type === 'email') {
-  //   //   // resetPasswordEmail({email: data, otp, newPassword});
-  //   // }
-  //   navigation.navigate('Login')
-  // };
 
   return (
     <SafeAreaScreen>
@@ -60,29 +56,31 @@ export default function ResetPassword ({
         icon={<BackButtonSvg />}
         onIconPress={() => navigation.goBack()}
       />
-       <KeyboardAvoidingView behavior='height'>
+      <KeyboardAvoidingView behavior='height'>
         <ScrollView showsVerticalScrollIndicator={false}>
-      <View style={styles.logoView}>
-        <VerificationSvg />
-      </View>
-      <View style={styles.textWrapper}>
-        <Text
-          variant='headlineMedium'
-          style={[styles.title, { color: colors.onBackground }]}
-        >
-          Create New Password
-        </Text>
-        <Text
-          variant='bodyMedium'
-          style={[styles.desc, { color: colors.onSecondary }]}
-        >
-          Your new password must be different from previously used password
-        </Text>
-      </View>
-
-          <ResetPasswordForm submitForm={submitForm} />
+          <View style={styles.logoView}>
+            <VerificationSvg />
+          </View>
+          <View style={styles.textWrapper}>
+            <Text
+              variant='headlineMedium'
+              style={[styles.title, { color: colors.onBackground }]}
+            >
+              Create New Password
+            </Text>
+            <Text
+              variant='bodyMedium'
+              style={[styles.desc, { color: colors.onSecondary }]}
+            >
+              Your new password must be different from previously used password
+            </Text>
+          </View>
+          <ResetPasswordForm
+            submitForm={submitForm}
+            loading={withPhoneLoading}
+          />
         </ScrollView>
-        </KeyboardAvoidingView>
+      </KeyboardAvoidingView>
     </SafeAreaScreen>
   );
 };
