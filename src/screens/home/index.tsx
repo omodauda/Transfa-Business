6cb8be266fd844d99310f60602464050cc2a2c05
omodauda <omodauda.dl@gmail.com>
2023-03-29 13:14:45 +0100

    render home screen based on status & data

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,11 +1,60 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import React from 'react'
+import { View, StyleSheet } from 'react-native';
+import { ActivityIndicator } from 'react-native-paper';
 import SafeAreaScreen from '~components/SafeAreaScreen'
+import useGetBusiness from '~hooks/api/useGetBusiness'
+import { BusinessReviewStatus } from '~__generated__/graphql';
+import MainView from './mainView';
 import PendingAccount from './pendingAccountView'
+import ZeroBikeView from './zeroBikeView';
 
 export default function Home() {
+  const { loading, data } = useGetBusiness();
+
+  function Loader() {
+    return (
+      <View style={[styles.loaderView]}>
+        <ActivityIndicator size='large' />
+      </View>
+    )
+  }
+
+  // eslint-disable-next-line consistent-return
+  function ScreenToRender() {
+    if (loading) {
+      return <Loader />;
+    }
+    if (!loading && data?.status === BusinessReviewStatus.Pending) {
+      return <PendingAccount />;
+    }
+    if (
+      !loading &&
+      data?.status === BusinessReviewStatus.Verified &&
+      !!data.riders
+    ) {
+      return <ZeroBikeView />
+    }
+    if (
+      !loading &&
+      data?.status === BusinessReviewStatus.Verified &&
+      data.riders
+    ) {
+      return <MainView />
+    }
+  }
+  
   return (
     <SafeAreaScreen style={{paddingHorizontal: 0}}>
-      <PendingAccount />
+      {ScreenToRender()}
     </SafeAreaScreen>
   )
 }
+
+const styles = StyleSheet.create({
+  loaderView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
