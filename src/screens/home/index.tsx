6858b4de8d92645aca264f2e637fbd24f5fee9d3
omodauda <omodauda.dl@gmail.com>
2023-04-01 14:55:46 +0100

    register device for push notification

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,17 +1,37 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, StyleSheet } from 'react-native';
 import { ActivityIndicator } from 'react-native-paper';
 import NetworkError from '~components/networkError';
 import SafeAreaScreen from '~components/SafeAreaScreen'
 import useGetBusiness from '~hooks/api/useGetBusiness'
 import { BusinessReviewStatus } from '~__generated__/graphql';
+import messaging from '@react-native-firebase/messaging'
+import useSaveFcmToken from '~hooks/api/useSaveFcmToken';
 import MainView from './mainView';
 import PendingAccount from './pendingAccountView'
 import ZeroBikeView from './zeroBikeView';
 
 export default function Home() {
   const { loading, data, error, refetch } = useGetBusiness();
+  const { saveToken } = useSaveFcmToken();
+
+  useEffect(() => {
+    const getFCMPermission = async () => {
+      /** register device for remote mesage */
+      const permission = await messaging().requestPermission();
+      const enabled = permission ===
+        messaging.AuthorizationStatus.AUTHORIZED || messaging.AuthorizationStatus.PROVISIONAL;
+      if (enabled) {
+        const token = await messaging().getToken();
+        saveToken({token})
+      }
+    };
+    getFCMPermission();
+    return messaging().onTokenRefresh(token => {
+      saveToken({token})
+    })
+  }, [])
 
   function Loader() {
     return (
