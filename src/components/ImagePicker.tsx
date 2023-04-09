105645c41a0e5e3100e700fac0e22b286ebcb4a3
omodauda <omodauda.dl@gmail.com>
2023-04-09 22:27:43 +0100

    image picker function

---
+++ src/components/ImagePicker.tsx
@@ -0,0 +1,28 @@
+import { SetStateAction } from "react";
+import * as ImagePicker from 'expo-image-picker';
+import { showMessage } from "react-native-flash-message";
+
+interface ImagePickerComponentprops {
+  setImage: (image: SetStateAction<ImagePicker.ImagePickerAsset | null>) => void;
+}
+
+const pickImage = async ({setImage}: ImagePickerComponentprops) => {
+  try {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: false,
+      quality: 1
+    })
+    if (!result.canceled) {
+      setImage(result.assets[0])
+    }
+  } catch (error) {
+    showMessage({
+      message: 'Error',
+      description: 'Unable to read selected image',
+      type: 'default',
+    });
+  }
+}
+
+export default pickImage
